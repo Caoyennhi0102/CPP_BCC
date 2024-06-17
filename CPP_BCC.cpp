3f81a3ec9b0d8436dcf34565644db8efc034dd12
Caoyennhi0102 <luuvinhphat1487@gamil.com>
2024-06-17 20:24:36 +0700

    Add project files.

---
+++ CPP_BCC.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int So; 
+	cout << "Nhập Số: ";
+	cin >> So;
+	for (int n = 2; n <= 9; n++) {
+		for (int i = 1; i <= 10; i++) {
+
+			cout << n << "x" << i << "=" << n * i << endl;
+		}
+	}
+
+	return 0;
+}
